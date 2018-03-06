fd1f287b497310ebea8b81afe0a2e1c2fcbdb547
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-03-06 22:38:12 +0900

    project init

---
+++ swift/vu-meter/vu-meter/VUMeter.swift
@@ -0,0 +1,21 @@
+//
+//  VUMeter.swift
+//  vu-meter
+//
+//  Created by 下村一将 on 2018/03/06.
+//  Copyright © 2018年 kazu. All rights reserved.
+//
+
+import UIKit
+
+class VUMeter: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
