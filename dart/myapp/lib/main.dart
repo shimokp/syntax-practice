c9f7298fd1a7fad7d680b621f0e54fbabc3e6034
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-03-07 22:55:36 +0900

    show words pair

--- dart/myapp/lib/main.dart
+++ dart/myapp/lib/main.dart
@@ -16,10 +16,34 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    // final wordPair = new WordPair.random();
+    // return new Text(wordPair.asPascalCase);
+    return _buildSuggestions();
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(itemBuilder: (buildContext, i) {
+      if (i.isOdd) return new Divider(); // 奇数
+
+      final index = i ~/ 2;
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[index]);
+    });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+        title: new Text(
+      pair.asPascalCase,
+      style: _biggerFont,
+    ));
   }
 }
 
