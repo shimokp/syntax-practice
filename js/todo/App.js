dc7e0a506acd703a977f55a6859b432b8172d3b0
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-06-10 22:46:41 +0900

    [todo]use state for manage todos

--- js/todo/App.js
+++ js/todo/App.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SectionList, Button, TextInput, FlatList } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [{ title: 'hoge' }, { title: 'hoge2' }] };
+    this.onPressAddTodo = this.onPressAddTodo.bind(this);
+  }
+
+  // getInitialState() {
+  //   console.log("getInitialState")
+  //   return {
+  //     todos: []
+  //   }
+  // }
+
+  onPressAddTodo(e) {
+    console.log("todos");
+
+    const { todos } = this.state;
+    todos.push({
+      title: this.state.text
+    });
+
+    console.log(todos);
+
+    this.setState({
+      todos: todos //this.state.todos//Object.assign(this.state.todos, { title: this.state.title })
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>
+          {"\n"}
+          {"\n"}
+        </Text>
+        <TextInput
+          backgroundColor="skyblue"
+          width={120}
+          ref={(el) => { this.text = el; }}
+          onChangeText={(text) => {
+            this.setState({ text })
+          }}
+          value={this.state.text}
+        //   ref
+        // onChangeText={(text) => {
+        //   console.log(text);
+        //   this.setState({ text: text })
+        //   console.log(this.state)
+        // }}
+        />
+        <Button title="Add" onPress={this.onPressAddTodo} />
+        <FlatList
+          data={this.state.todos}
+          execData={this.state.todos}
+          renderItem={({ item }) => <Text>{item.title}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        // removeClippedSubviews={false}
+        />
       </View>
     );
   }
