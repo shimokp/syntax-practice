601a8005566922a0346b7a12528349790c607204
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-06-12 21:02:50 +0900

    able to only show

--- js/react-intro-book/todo/src/App.js
+++ js/react-intro-book/todo/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
 
 class App extends Component {
   render() {
+    const tasks = [
+      { title: 'first task', id: 0 },
+      { title: 'second task', id: 1 },
+    ]
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>TODO App</h1>
+        <TodoInput />
+        <TodoList tasks={tasks} />
       </div>
     );
   }
