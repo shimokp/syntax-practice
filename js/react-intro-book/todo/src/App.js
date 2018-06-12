e6ae741f57ada4a1a9700b4c8c4142ddbe214cd9
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-06-12 21:40:24 +0900

    manage todo

--- js/react-intro-book/todo/src/App.js
+++ js/react-intro-book/todo/src/App.js
@@ -3,16 +3,49 @@ import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tasks: [
+        { title: 'first task', id: 0 },
+        { title: 'second task', id: 1 },
+      ],
+      uniqueId: 1,
+    }
+    this.addTodo = this.addTodo.bind(this)
+    this.resetTodo = this.resetTodo.bind(this)
+  }
+
+  addTodo(title) {
+    const {
+      tasks,
+      uniqueId
+    } = this.state
+
+    tasks.push({
+      title,
+      id: uniqueId
+    })
+
+    this.setState({
+      tasks,
+      uniqueId: uniqueId + 1,
+    })
+  }
+
+  resetTodo() {
+    this.setState({
+      tasks: []
+    })
+  }
+
   render() {
-    const tasks = [
-      { title: 'first task', id: 0 },
-      { title: 'second task', id: 1 },
-    ]
     return (
       <div>
         <h1>TODO App</h1>
-        <TodoInput />
-        <TodoList tasks={tasks} />
+        <button onClick={this.resetTodo} >Reset</button>
+        <TodoInput addTodo={this.addTodo} />
+        <TodoList tasks={this.state.tasks} />
       </div>
     );
   }
--- js/react-intro-book/todo/src/components/TodoInput.js
+++ js/react-intro-book/todo/src/components/TodoInput.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class TodoInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleChange(e) {
+    this.setState({
+      inputValue: e.target.value,
+    })
+  }
+  handleClick() {
+    this.props.addTodo(this.state.inputValue)
+  }
   render() {
     return (
       <div>
-        <input placeholder='新規TODOを入力してください' />
-        <button>登録</button>
+        <input placeholder='新規TODOを入力してください' value={this.state.inputValue} onChange={this.handleChange} />
+        <button onClick={this.handleClick}>登録</button>
       </div>
     )
   }
