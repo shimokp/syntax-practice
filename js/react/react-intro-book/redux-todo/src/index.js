442a3129387d23ff50c93ab3b744411f4ead12e9
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-06-12 23:13:45 +0900

    subscribe store

--- js/react/react-intro-book/redux-todo/src/index.js
+++ js/react/react-intro-book/redux-todo/src/index.js
@@ -3,8 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 
+const initialState = {
+    tasks: []
+}
 
+function tasksReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'ADD_TASK':
+            return {
+                ...state,
+                tasks: state.tasks.concat([action.payload.task])
+            }
+        default:
+            return state
+    }
+}
+
+const addTask = (task) => ({
+    type: 'ADD_TASK',
+    payload: {
+        task
+    }
+})
+
+const store = createStore(tasksReducer)
+function handleChange() {
+    console.log('handle change')
+    console.log(store.getState())
+}
+
+const unsubscribe = store.subscribe(handleChange)
+
+console.log(store.getState())
+store.dispatch(addTask('Store learning'))
+store.dispatch(addTask('Store learning'))
+store.dispatch(addTask('Store learning'))
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
