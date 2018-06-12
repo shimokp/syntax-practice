d364ead70c61667391631bf8dbe6904eeeb097a2
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-06-12 20:27:54 +0900

    progress

--- js/react-intro-book/my-app/src/App.js
+++ js/react-intro-book/my-app/src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const List = () => {
+  return <ul>
+    {[1, 2, 3].map(num => <li>{num}</li>)}
+  </ul>
+}
+
+const Hello = (props) => {
+  return <div>Hello, {props.children}!!!</div>
+}
 class App extends Component {
   render() {
     return (
@@ -13,6 +22,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <List />
+        <Hello children='hogehoge'>
+        </Hello>
       </div>
     );
   }
