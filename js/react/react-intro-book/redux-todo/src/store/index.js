458da20ad9d8f4535d14915bf32985b11970d3c9
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-06-15 00:35:41 +0900

    use middleware

--- js/react/react-intro-book/redux-todo/src/store/index.js
+++ js/react/react-intro-book/redux-todo/src/store/index.js
@@ -5,6 +5,7 @@ import {
 } from 'redux';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import tasksReducer from '../reducers/tasks';
+import { createLogger } from 'redux-logger';
 
 export default function createStore(history) {
     return reduxCreateStore(
@@ -13,7 +14,10 @@ export default function createStore(history) {
             router: routerReducer,
         }),
         applyMiddleware( //ReduxのAction経由でルーティングを制御できるようにする
-            routerMiddleware(history)
+            routerMiddleware(history),
+            createLogger({
+                predicate: (getState, action) => action.type !== 'INPUT_TASK' // mute the log of noisy action
+            })
         )
     )
 }
