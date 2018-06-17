930c12e8a42aa00597f0ee07ad963b5ac54e9090
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-06-17 23:14:20 +0900

    beautiful ui

--- js/react/react-intro-book/redux-todo/src/components/TodoApp.js
+++ js/react/react-intro-book/redux-todo/src/components/TodoApp.js
@@ -1,25 +1,44 @@
 import React from 'react';
+import Reboot from 'material-ui/Reboot';
+import Button from 'material-ui/Button';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Input from 'material-ui/Input';
+import List, { ListItem, ListItemText } from 'material-ui/List';
 
 export default function TodoApp({ taskTitle, tasks, inputTask, addTask, resetTask, redirectToError, asyncAddTask }) {
     return (
         <div>
-            <input type='text' onChange={(e) => inputTask(e.target.value)} value={taskTitle} />
-            <input type='button' value='Add' onClick={() => addTask(taskTitle)} />
-            <input type='button' value='Async Add' onClick={() => {
-                console.log(asyncAddTask)
-                asyncAddTask(taskTitle)
-            }} />
-            <button onClick={() => resetTask()} >Reset</button>
-            <ul>
-                {
-                    tasks.map(function (item, i) {
-                        return (
-                            <li key={i}>{item.title}</li>
-                        )
-                    })
-                }
-            </ul>
-            <button onClick={() => redirectToError()} >Go to Error</button>
+            <Reboot />
+            <AppBar position='static'>
+                <Toolbar>
+                    <Typography type='title' color='inherit'>
+                        Todo
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <div style={{ padding: 16 }}>
+                <Input onChange={(e) => inputTask(e.target.value)} value={taskTitle} />
+                <Button raised color='primary' onClick={() => addTask(taskTitle)}>Add</Button>
+                <Button raised color='secondary' onClick={() => {
+                    console.log(asyncAddTask)
+                    asyncAddTask(taskTitle)
+                }}>Async Add</Button>
+                <Button raised color='inherit' onClick={() => resetTask()}>Reset</Button>
+                <List>
+                    {
+                        tasks.map(function (item, i) {
+                            return (
+                                <ListItem key={i}>
+                                    <ListItemText primary={`・${item.title}`} />
+                                </ListItem>
+                            )
+                        })
+                    }
+                </List>
+                <button onClick={() => redirectToError()} >Go to Error</button>
+            </div>
         </div>
     )
 }
