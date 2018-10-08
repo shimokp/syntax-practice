25fa431e236b2c6d9a0780bd8ac710bd239f9b25
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-10-08 23:19:03 +0900

    init

---
+++ swift/tdd-practice/tdd-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tdd-practice
+//
+//  Created by 下村一将 on 2018/10/08.
+//  Copyright © 2018 下村一将. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ swift/tdd-practice/tdd-practiceTests/tdd_practiceTests.swift
@@ -0,0 +1,33 @@
+//
+//  tdd_practiceTests.swift
+//  tdd-practiceTests
+//
+//  Created by 下村一将 on 2018/10/08.
+//  Copyright © 2018 下村一将. All rights reserved.
+//
+
+import XCTest
+
+class tdd_practiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
