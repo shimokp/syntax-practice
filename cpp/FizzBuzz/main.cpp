a9ca3e857c272d5ea6475f68d3d2e780adadeb89
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2020-05-20 09:56:21 +0900

    Add FizzBuzz for cpp

--- cpp/FizzBuzz/main.cpp
+++ cpp/FizzBuzz/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello! World!" << std::endl;
+  for (int i = 1; i <= 30; i++) {
+    std::string output;
+
+    if (i % (3 * 5) == 0)
+      output = "FizzBuzz";
+    else if (i % 3 == 0)
+      output = "Fizz";
+    else if (i % 5 == 0)
+      output = "Buzz";
+    else
+      output = std::to_string(i);
+
+    std::cout << output << std::endl;
+  }
+
   return 0;
 }
