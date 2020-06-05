e79000cd67f5409d9951808306801d8ccf2d978a
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2020-06-06 01:32:05 +0900

    Use filter

--- rust/sample-web-app/src/main.rs
+++ rust/sample-web-app/src/main.rs
@@ -1,7 +1,20 @@
-use warp::Filter;
+use warp::{ Filter, Reply, Rejection };
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let hello = hello().and(name()).and_then(greet_hander);
     warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn hello() -> warp::filters::BoxedFilter<()> {
+    warp::path("hello").boxed()
+}
+
+fn name() -> warp::filters::BoxedFilter<(String,)> {
+    warp::path::param().boxed()
+}
+
+async fn greet_hander(name: String) -> Result<impl Reply, Rejection> {
+    let reply = format!("hello {}", name);
+    Ok(warp::reply::html(reply))
 }
